f58a5a50519dfeff93f067c5520423c8c44719d4
Vin Rodriguez <vince.ray90@gmail.com>
2021-03-28 18:05:53 -0500

    created js files and included packages for the application

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const readline = require('readline').createinterface({
+    input: process.stdin, 
+    output: process.stdout,
+});
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
